d8a979420eb18280222bc45a24c0e0df94ad4ee0
Andrei Hilevich <andrei.hilevich@soft-fx.lv>
2015-10-13 11:17:44 +0000

    Algo: Mock indicator project

---
+++ TestAlgoProject/SimpleMovingAverage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TickTrader.Algo.Api;
+
+namespace TestAlgoProject
+{
+    [Indicator]
+    public class SimpleMovingAverage : Indicator
+    {
+        [Parameter]
+        public int Range { get; set; }
+
+        [Input]
+        public DataSeries Input { get; set; }
+
+        [Output]
+        public DataSeries Output { get; set; }
+
+        protected override void Calculate()
+        {
+            Output[0] = Input.Take(Range).Average();
+        }
+    }
+}
