5df425b72ea7dfbb825de17b0f8b5d39f6cf7e0e
ahilevich <andrei.hilevich@soft-fx.lv>
2016-03-03 20:10:55 +0200

    Algo: Fix compile error

--- TickTrader.Algo.Indicators.UTest/PorabolicTest/PorabolicTest.cs
+++ TickTrader.Algo.Indicators.UTest/PorabolicTest/PorabolicTest.cs
@@ -38,7 +38,7 @@ namespace TickTrader.Algo.Indicators.UTest.PorabolicTest
             writer.AddMapping("ExtSARBuffer", testResPor);
 
 
-            builder = new IndicatorBuilder<Bar>(typeof(Porabolic.Porabolic), reader, writer);
+            builder = new IndicatorBuilder<Bar>(typeof(Parabolic.Parabolic), reader, writer);
             builder.SetParameter("InpSARStep", 0.02);
             builder.SetParameter("InpSARMaximum", 0.2);
 
@@ -77,7 +77,7 @@ namespace TickTrader.Algo.Indicators.UTest.PorabolicTest
             writer.AddMapping("ExtSARBuffer", testResPor);
 
 
-            builder = new IndicatorBuilder<Bar>(typeof(Porabolic.Porabolic), reader, writer);
+            builder = new IndicatorBuilder<Bar>(typeof(Parabolic.Parabolic), reader, writer);
             builder.SetParameter("InpSARStep", 0.02);
             builder.SetParameter("InpSARMaximum", 0.2);
 
@@ -117,7 +117,7 @@ namespace TickTrader.Algo.Indicators.UTest.PorabolicTest
             writer.AddMapping("ExtSARBuffer", testResPor);
 
 
-            builder = new IndicatorBuilder<Bar>(typeof(Porabolic.Porabolic), reader, writer);
+            builder = new IndicatorBuilder<Bar>(typeof(Parabolic.Parabolic), reader, writer);
             builder.SetParameter("InpSARStep", 0.02);
             builder.SetParameter("InpSARMaximum", 0.2);
 
@@ -156,7 +156,7 @@ namespace TickTrader.Algo.Indicators.UTest.PorabolicTest
             writer.AddMapping("ExtSARBuffer", testResPor);
 
 
-            builder = new IndicatorBuilder<Bar>(typeof(Porabolic.Porabolic), reader, writer);
+            builder = new IndicatorBuilder<Bar>(typeof(Parabolic.Parabolic), reader, writer);
             builder.SetParameter("InpSARStep", 0.02);
             builder.SetParameter("InpSARMaximum", 0.2);
 
