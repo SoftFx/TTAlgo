9e8d8c40f8e3030202abe5189fac49c676ac9f91
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2016-04-11 11:39:11 +0300

    Added following Indicators:  - Average True Range  - Bears Power  - Bulls Power  - Commodity Channel Index  - DeMarker

---
+++ TickTrader.Algo.Indicators/Oscillators/CommodityChannelIndex/CommodityChannelIndex.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using TickTrader.Algo.Api;
+using TickTrader.Algo.Indicators.Trend.MovingAverage;
+using TickTrader.Algo.Indicators.Utility;
+
+namespace TickTrader.Algo.Indicators.Oscillators.CommodityChannelIndex
+{
+    [Indicator(Category = "Oscillators", DisplayName = "Oscillators/Commodity Channel Index")]
+    public class CommodityChannelIndex : Indicator
+    {
+        private MovingAverage _sma;
+        private IMA _meanDeviation;
+
+        [Parameter(DefaultValue = 14, DisplayName = "Period")]
+        public int Period { get; set; }
+
+        private AppliedPrice.Target _targetPrice;
+        [Parameter(DefaultValue = 5, DisplayName = "Apply To")]
+        public int TargetPrice
+        {
+            get { return (int)_targetPrice; }
+            set { _targetPrice = (AppliedPrice.Target)value; }
+        }
+
+        [Input]
+        public DataSeries<Bar> Bars { get; set; }
+
+        [Output(DisplayName = "CCI", DefaultColor = Colors.LightSeaGreen)]
+        public DataSeries Cci { get; set; }
+
+        public int LastPositionChanged { get { return _sma.LastPositionChanged; } }
+
+        public CommodityChannelIndex() { }
+
+        public CommodityChannelIndex(DataSeries<Bar> bars, int period, AppliedPrice.Target targetPrice)
+        {
+            Bars = bars;
+            Period = period;
+            _targetPrice = targetPrice;
+
+            InitializeIndicator();
+        }
+
+        protected void InitializeIndicator()
+        {
+            _sma = new MovingAverage(Bars, Period, 0, Method.Simple, _targetPrice);
+            _meanDeviation = MABase.CreateMaInstance(Period, Method.Simple);
+            _meanDeviation.Init();
+        }
+
+        protected override void Init()
+        {
+            InitializeIndicator();
+        }
+
+        protected override void Calculate()
+        {
+            var pos = LastPositionChanged;
+            var appliedPrice = AppliedPrice.Calculate(Bars[pos], _targetPrice);
+            var val = double.IsNaN(_sma.Average[pos]) ? appliedPrice : _sma.Average[pos] - appliedPrice;
+            if (IsUpdate)
+            {
+                _meanDeviation.UpdateLast(Math.Abs(val));
+            }
+            else
+            {
+                _meanDeviation.Add(Math.Abs(val));
+            }
+            Cci[pos] = (appliedPrice - _sma.Average[pos])/(0.015*_meanDeviation.Average);
+        }
+    }
+}
---
+++ TickTrader.Algo.Indicators/Oscillators/DeMarker/DeMarker.cs
@@ -0,0 +1,67 @@
+﻿using TickTrader.Algo.Api;
+using TickTrader.Algo.Indicators.Trend.MovingAverage;
+
+namespace TickTrader.Algo.Indicators.Oscillators.DeMarker
+{
+    [Indicator(Category = "Oscillators", DisplayName = "Oscillators/DeMarker")]
+    public class DeMarker : Indicator
+    {
+        private IMA _smaDeMax, _smaDeMin;
+
+        [Parameter(DefaultValue = 14, DisplayName = "Period")]
+        public int Period { get; set; }
+
+        [Input]
+        public DataSeries<Bar> Bars { get; set; }
+
+        [Output(DisplayName = "DeMarker", DefaultColor = Colors.LightSeaGreen)]
+        public DataSeries DeMark { get; set; }
+
+        public int LastPositionChanged { get { return 0; } }
+
+        public DeMarker() { }
+
+        public DeMarker(DataSeries<Bar> bars, int period)
+        {
+            Bars = bars;
+            Period = period;
+
+            InitializeIndicator();
+        }
+
+        protected void InitializeIndicator()
+        {
+            _smaDeMax = MABase.CreateMaInstance(Period, Method.Simple);
+            _smaDeMax.Init();
+            _smaDeMin = MABase.CreateMaInstance(Period, Method.Simple);
+            _smaDeMin.Init();
+        }
+
+        protected override void Init()
+        {
+            InitializeIndicator();
+        }
+
+        protected override void Calculate()
+        {
+            if (Bars.Count < 2)
+            {
+                DeMark[LastPositionChanged] = double.NaN;
+                return;
+            }
+            var deMax = Bars[0].High > Bars[1].High ? Bars[0].High - Bars[1].High : 0.0;
+            var deMin = Bars[0].Low < Bars[1].Low ? Bars[1].Low - Bars[0].Low : 0.0;
+            if (IsUpdate)
+            {
+                _smaDeMax.UpdateLast(deMax);
+                _smaDeMin.UpdateLast(deMin);
+            }
+            else
+            {
+                _smaDeMax.Add(deMax);
+                _smaDeMin.Add(deMin);
+            }
+            DeMark[LastPositionChanged] = _smaDeMax.Average/(_smaDeMax.Average + _smaDeMin.Average);
+        }
+    }
+}
