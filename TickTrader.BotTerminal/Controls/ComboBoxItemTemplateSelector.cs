b14116415b2885cb716bc627410c986b4b62f2a6
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-05-05 12:38:12 +0300

    Added ComboBoxItemTemplateSelector

---
+++ TickTrader.BotTerminal/Controls/ComboBoxItemTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TickTrader.BotTerminal
+{
+    internal class ComboBoxItemTemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate SelectedItemTemplate { get; set; }
+        public DataTemplate ItemTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            bool selected = true;
+            
+            FrameworkElement fe = container as FrameworkElement;
+            if (fe != null)
+            {
+                DependencyObject parent = fe.TemplatedParent;
+                if (parent != null)
+                {
+                    ComboBoxItem cbi = parent as ComboBoxItem;
+                    if (cbi != null)
+                        selected = false;
+                }
+            }
+
+            if (selected)
+                return SelectedItemTemplate;
+            else
+                return ItemTemplate;
+        }
+    }
+}
