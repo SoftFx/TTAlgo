301e9525a979ded72f49ab8a0f76af47c10fb655
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-05-05 17:23:47 +0300

    added feature to set line style of indicator

---
+++ TickTrader.BotTerminal/Converters/LineStyleToDashArrayConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using TickTrader.Algo.Api;
+
+namespace TickTrader.BotTerminal
+{
+    [ValueConversion(sourceType: typeof(LineStyles), targetType: typeof(double[]))]
+    class LineStyleToDashArrayConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return  new DoubleCollection(((LineStyles)value).ToStrokeDashArray());
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
