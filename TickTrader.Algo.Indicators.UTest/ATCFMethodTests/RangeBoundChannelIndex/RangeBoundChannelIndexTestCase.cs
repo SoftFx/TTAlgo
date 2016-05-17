92f9d23575393cedabbee2da9428a34442b71e93
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2016-05-17 21:14:14 +0300

    Implemented Range Bound Channel Index Indicator. Added more tests for Range Channel Bound Index Indicator.

--- TickTrader.Algo.Indicators.UTest/ATCFMethodTests/RangeBoundChannelIndex/RangeBoundChannelIndexTestCase.cs
+++ TickTrader.Algo.Indicators.UTest/ATCFMethodTests/RangeBoundChannelIndex/RangeBoundChannelIndexTestCase.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TickTrader.Algo.Indicators.Utility;
 using TickTrader.Algo.Indicators.UTest.TestCases;
 using TickTrader.Algo.Indicators.UTest.Utility;
@@ -8,16 +10,18 @@ namespace TickTrader.Algo.Indicators.UTest.ATCFMethodTests.RangeBoundChannelInde
     public class RangeBoundChannelIndexTestCase : DigitalIndicatorTestCase
     {
         public int Std { get; protected set; }
+        public int CountBars { get; protected set; }
 
         public RangeBoundChannelIndexTestCase(Type indicatorType, string symbol, string quotesPath, string answerPath,
-            int std) : base(indicatorType, symbol, quotesPath, answerPath, 40)
+            int std, int countBars) : base(indicatorType, symbol, quotesPath, answerPath, 40)
         {
             Std = std;
+            CountBars = countBars;
         }
 
         protected override void SetupParameters()
         {
-            base.SetupParameters();
+            SetParameter("CountBars", CountBars);
             SetParameter("Std", Std);
         }
 
@@ -33,6 +37,18 @@ namespace TickTrader.Algo.Indicators.UTest.ATCFMethodTests.RangeBoundChannelInde
             PutOutputToBuffer("LowerBound", 2);
             PutOutputToBuffer("UpperBound2", 3);
             PutOutputToBuffer("LowerBound2", 4);
+        }
+
+        protected override void CheckAnswerUnit(int index, List<double>[] metaAnswer)
+        {
+            for (var k = 0; k < AnswerUnitSize / 8; k++)
+            {
+                metaAnswer[k][index] = Math.Abs(metaAnswer[k][index] - int.MaxValue) < 1e-20 ? 0 : metaAnswer[k][index];
+                AnswerBuffer[k][index] = double.IsNaN(AnswerBuffer[k][index])
+                    ? 0
+                    : AnswerBuffer[k][index];
+                AssertX.Greater(Epsilon, Math.Abs(metaAnswer[k][index] - AnswerBuffer[k][index]));
+            }
         }
     }
 }
