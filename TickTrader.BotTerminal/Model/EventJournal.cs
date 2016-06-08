bc3489eaca96b4f91922eff57aba3b3b314f84f3
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-06-08 14:14:56 +0300

    bug fixes

--- TickTrader.BotTerminal/Model/EventJournal.cs
+++ TickTrader.BotTerminal/Model/EventJournal.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace TickTrader.BotTerminal
 {
@@ -13,15 +14,17 @@ namespace TickTrader.BotTerminal
     {
         private readonly int firstItem = 0;
         private int journalSize;
-        private BindableCollection<JournalItem> items;
+        private ObservableCollection<JournalItem> items;
         private Logger logger;
+        private object syncObj = new object();
 
         public EventJournal(int journalSize = 200)
         {
             this.journalSize = journalSize;
-            this.items = new BindableCollection<JournalItem>();
+            this.items = new ObservableCollection<JournalItem>();
             this.Events = new ReadOnlyObservableCollection<JournalItem>(items);
             this.logger = NLog.LogManager.GetCurrentClassLogger();
+            BindingOperations.EnableCollectionSynchronization(Events, syncObj);
         }
 
         private bool IsJournalFull { get { return items.Count >= journalSize; } }
@@ -34,16 +37,22 @@ namespace TickTrader.BotTerminal
 
         public void Add(string message, params object[] args)
         {
-            if (IsJournalFull)
-                items.RemoveAt(firstItem);
+            lock (syncObj)
+            {
+                if (IsJournalFull)
+                    items.RemoveAt(firstItem);
 
-            items.Add(new JournalItem(string.Format(message, args)));
+                items.Add(new JournalItem(string.Format(message, args)));
+            }
             logger.Info(message, args);
         }
 
         public void Clear()
         {
-            items.Clear();
+            lock (syncObj)
+            {
+                items.Clear();
+            }
         }
     }
 
