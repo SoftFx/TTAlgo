bb878a5ef101c6bcb7b2a0c25180c5651985f1a5
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-06-30 14:50:48 +0300

    Drag&Drop improvements

---
+++ TickTrader.BotTerminal/Behaviors/DragDrop/PlatformProxy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TickTrader.BotTerminal
+{
+    internal class PlatformProxy
+    {
+        const uint GW_HWNDNEXT = 2;
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int WS_EX_LAYERED = 0x00080000;
+        const int GWL_EXSTYLE = (-20);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        [DllImport("User32")]
+        static extern IntPtr GetTopWindow(IntPtr hWnd);
+        [DllImport("User32")]
+        static extern IntPtr GetWindow(IntPtr hWnd, uint wCmd);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(ref Win32Point pt);
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hwnd, int index);
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        public static void MakeTransparent(IntPtr hwnd)
+        {
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+    }
+}
