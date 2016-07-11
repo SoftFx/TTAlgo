e48a31a70a05ff62bc8439e4135e426539b19e16
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-07-11 15:07:08 +0300

    Added exit dialog & ui blocker

---
+++ TickTrader.BotTerminal/Model/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTrader.BotTerminal
+{
+    internal class UI
+    {
+        private static UI instance = new UI();
+
+        private UI() { }
+
+        public static UI Instance { get { return instance; } }
+        public bool Locked { get; private set; }
+
+        public void Lock()
+        {
+            Locked = true;
+            StateChanged();
+        }
+
+        public void Release()
+        {
+            Locked = false;
+            StateChanged();
+        }
+
+        public event Action StateChanged = delegate { };
+    }
+}
