7303ba145ec81ef3b515dff2600d023398137909
ahilevich <andrei.hilevich@soft-fx.lv>
2016-09-01 17:45:12 +0300

    Market maker Bot Stub

--- TestAlgoProject/Bots/MMBot.cs
+++ TestAlgoProject/Bots/MMBot.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nett;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,32 @@ namespace TestAlgoProject.Bots
     [TradeBot(DisplayName = "Market Maker")]
     public class MMBot : TradeBot
     {
-        [Parameter(DisplayName = "Configuration File")]
-        [FileFilter("Doc files (*.docx)", "*.docx")]
+        [Parameter(DisplayName = "Symbol Config File")]
+        [FileFilter("Toml Config (*.tml)", "*.tml")]
         public File Config { get; set; }
 
-        [Parameter]
-        public int P1 { get; set; }
+        [Parameter(DisplayName = "Volume Multiplier", DefaultValue = 1D)]
+        public double Multiplier { get; set; }
 
-        [Parameter]
-        public int P2 { get; set; }
+        protected override void Init()
+        {
+            try
+            {
+                if (Config.IsNull)
+                    throw new Exception("Config file is not specified!");
+
+                var table = Nett.Toml.ReadFile(Config.FullPath);
+                var combinations = table.Get<TomlTable>("COMBINATIONS");
+                var volumes = table.Get<TomlTable>("VOLUMES");
+
+                foreach (var row in combinations.Rows)
+                    Print(row.Key + " = " + row.Value.Get<string>());
+            }
+            catch (Exception ex)
+            {
+                PrintError(ex.Message);
+                Exit();
+            }
+        }
     }
 }
