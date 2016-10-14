1cfc331c6531b343e16f19c59f47b218f371af7e
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-10-14 15:05:22 +0300

    some changes and fixes in styles (PopupButton, TextBox, FlatRadioButton)

--- TickTrader.BotTerminal/Controls/PopupButton.cs
+++ TickTrader.BotTerminal/Controls/PopupButton.cs
@@ -5,29 +5,77 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace TickTrader.BotTerminal
 {
     public class PopupButton: ContentControl
     {
+
+        public static DependencyProperty HighlightBackgroundProperty =
+         DependencyProperty.Register(nameof(HighlightBackground), typeof(Brush), typeof(PopupButton));
+
+        public static DependencyProperty DisabledBackgroundProperty =
+            DependencyProperty.Register(nameof(DisabledBackground), typeof(Brush), typeof(PopupButton));
+
+        public static DependencyProperty CheckedBackgroundProperty =
+            DependencyProperty.Register(nameof(CheckedBackground), typeof(Brush), typeof(PopupButton));
+
+        public static DependencyProperty HighlightForegroundProperty =
+        DependencyProperty.Register(nameof(HighlightForeground), typeof(Brush), typeof(PopupButton));
+
+        public static DependencyProperty DisabledForegroundProperty =
+            DependencyProperty.Register(nameof(DisabledForeground), typeof(Brush), typeof(PopupButton));
+
+        public static DependencyProperty CheckedForegroundProperty =
+            DependencyProperty.Register(nameof(CheckedForeground), typeof(Brush), typeof(PopupButton));
+
+
+        public static readonly DependencyProperty PopupContentProperty =
+          DependencyProperty.Register(nameof(PopupContent), typeof(object), typeof(PopupButton));
+
+
         public object PopupContent
         {
             get { return (object)GetValue(PopupContentProperty); }
             set { SetValue(PopupContentProperty, value); }
         }
 
-        public static readonly DependencyProperty PopupContentProperty =
-            DependencyProperty.Register(nameof(PopupContent), typeof(object), typeof(PopupButton));
-
-
-        public bool IsDropDownOpen
+        public Brush HighlightBackground
         {
-            get { return (bool)GetValue(IsDropDownOpenProperty); }
-            set { SetValue(IsDropDownOpenProperty, value); }
+            get { return (Brush)GetValue(HighlightBackgroundProperty); }
+            set { SetValue(HighlightBackgroundProperty, value); }
         }
 
-        public static readonly DependencyProperty IsDropDownOpenProperty =
-            DependencyProperty.Register(nameof(IsDropDownOpen), typeof(bool), typeof(PopupButton));
+        public Brush DisabledBackground
+        {
+            get { return (Brush)GetValue(DisabledBackgroundProperty); }
+            set { SetValue(DisabledBackgroundProperty, value); }
+        }
+
+        public Brush CheckedBackground
+        {
+            get { return (Brush)GetValue(CheckedBackgroundProperty); }
+            set { SetValue(CheckedBackgroundProperty, value); }
+        }
+
+        public Brush HighlightForeground
+        {
+            get { return (Brush)GetValue(HighlightForegroundProperty); }
+            set { SetValue(HighlightForegroundProperty, value); }
+        }
+
+        public Brush DisabledForeground
+        {
+            get { return (Brush)GetValue(DisabledForegroundProperty); }
+            set { SetValue(DisabledForegroundProperty, value); }
+        }
+
+        public Brush CheckedForeground
+        {
+            get { return (Brush)GetValue(CheckedForegroundProperty); }
+            set { SetValue(CheckedForegroundProperty, value); }
+        }
 
 
     }
