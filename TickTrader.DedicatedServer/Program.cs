3040f7158ad8d35cb1cbbecdee9be2c7a5e3a3ab
ahilevich <andrei.hilevich@soft-fx.lv>
2016-12-20 17:45:19 +0200

    Fix bug: bot setup dialog crashes if a bot has file parameter

---
+++ TickTrader.DedicatedServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TickTrader.DedicatedServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Web.WebHost host = new Web.WebHost();
+            host.Start();
+
+            Console.Read();
+
+            host.Stop();
+        }
+    }
+}
