3064244355f538dbf4879a38cc49602cb1d5442a
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2016-12-20 15:29:10 +0200

    some fixes after rename project

--- TickTrader.DedicatedServer.Web/Program.cs
+++ TickTrader.DedicatedServer.Web/Program.cs
@@ -2,7 +2,7 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
-namespace TickTrader.DedicatedServer
+namespace TickTrader.DedicatedServer.Web
 {
     public class Program
     {
