22073d8008a9e04490da8d70ff3ee281fa2b6a35
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2017-02-14 13:13:04 +0300

    Fixed assets update on order and balance operations.

--- TickTrader.Algo.Core/Entities/OrderExecReport.cs
+++ TickTrader.Algo.Core/Entities/OrderExecReport.cs
@@ -11,7 +11,7 @@ namespace TickTrader.Algo.Core
         public double? NewBalance { get; set; }
         public OrderExecAction ExecAction { get; set; }
         public OrderEntityAction Action { get; set; }
-        public List<AssetEntity> Assets { get; }
+        public List<AssetEntity> Assets { get; set; }
     }
 
     public enum OrderExecAction { Opened, Modified, Canceled, Closed, Filled, Expired }
