205295d60c574e8035d91dcc7ac14b0b422c55d1
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2017-04-10 10:28:41 +0300

    clean project + fix webpack issue

--- TickTrader.DedicatedServer/WebAdmin/Client/admin/accounts/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./account-card.component"));
-__export(require("./accounts.component"));
-__export(require("./change-password.component"));
-__export(require("./test-account.component"));
-//# sourceMappingURL=index.js.map
