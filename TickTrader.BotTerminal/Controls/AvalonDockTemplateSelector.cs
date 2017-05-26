54abd72ca717c7e896e46e58bb70db6e1aa8b746
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2017-05-26 13:23:46 +0300

    TTALGO-412: Added Trades and History records quantity display.

--- TickTrader.BotTerminal/Controls/AvalonDockTemplateSelector.cs
+++ TickTrader.BotTerminal/Controls/AvalonDockTemplateSelector.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using Xceed.Wpf.AvalonDock.Layout;
 
 namespace TickTrader.BotTerminal
 {
@@ -34,6 +35,35 @@ namespace TickTrader.BotTerminal
                 return null;
 
             return DocumentStyle;
+        }
+    }
+
+    public class AvalonAnchorTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate AnchorTemplate { get; set; }
+
+        public DataTemplate TradesTemplate { get; set; }
+
+        public DataTemplate HistoryTemplate { get; set; }
+
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var a = item as LayoutAnchorable;
+
+            if (a != null)
+            {
+                if (a.Title == ResxCore.Instance["Tab_Trade"] as string)
+                {
+                    return TradesTemplate;
+                }
+                if (a.Title == ResxCore.Instance["Tab_History"] as string)
+                {
+                    return HistoryTemplate;
+                }
+            }
+
+            return AnchorTemplate;
         }
     }
 }
