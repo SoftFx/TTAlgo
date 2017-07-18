4b264ab7701093d4c9a1f65027a80c47062cbb9f
Alexandr Roskoshanskii <alex.roskoshansky@soft-fx.lv>
2017-07-18 13:05:40 +0300

    partial adaptation side & top menu for angular

---
+++ TickTrader.DedicatedServer/WebAdmin/Client/logout.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from './services/index';
+import { AuthCredentials } from './models/index';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'logout',
+    template: require('./logout.component.html')
+})
+
+export class LogoutComponent {
+    public LogInStatusMessage: string;
+    public Creds: AuthCredentials = new AuthCredentials();
+
+    constructor(private _api: ApiService) {
+    }
+
+    ngOnInit() {
+        this._api.Auth.LogOut();
+    }
+}
---
+++ TickTrader.DedicatedServer/WebAdmin/Client/services/navigation.service.ts
@@ -0,0 +1,38 @@
+﻿import { Injectable } from '@angular/core';
+import { MenuType, RouteInfo, Action } from '../models/index';
+
+@Injectable()
+export class NavigationService {
+
+    SideBarOpened: boolean;
+
+    Routes: RouteInfo[] = [
+        { path: '/dashboard', title: 'Dashboard', menuType: MenuType.SideBar, icon: 'fa fa-tachometer', owner: null },
+        { path: '/configurate', title: 'Add Bot', menuType: MenuType.NavBar, icon: '', owner: "/dashboard" },
+        { path: '/repository', title: 'Repository', menuType: MenuType.SideBar, icon: 'fa fa-cloud', owner: null },
+        { path: '/accounts', title: 'Accounts', menuType: MenuType.SideBar, icon: 'fa fa-users', owner: null },
+        { path: '/logout', title: 'Log Out', menuType: MenuType.NavBar, icon: 'fa fa-sign-out', owner: null },
+    ];
+
+    TopMenuItems(): RouteInfo[]{
+        var titlee = window.location.pathname;
+
+        return this.Routes.filter(menuItem => menuItem.menuType === MenuType.NavBar && menuItem.owner === titlee)
+            .concat(this.Routes.filter(menuItem => menuItem.menuType === MenuType.NavBar && menuItem.owner == null));
+    }
+
+    SideMenuItems(): RouteInfo[] {
+        return this.Routes.filter(menuItem => menuItem.menuType === MenuType.SideBar);
+    }
+
+    ToggleSideBar(): void {
+        this.SideBarOpened = !this.SideBarOpened;
+    }
+
+    IsMobileVersion() {
+        if ($(window).width() > 991) {
+            return false;
+        }
+        return true;
+    }
+}
