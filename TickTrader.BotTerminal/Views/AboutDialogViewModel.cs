a1abb1bbdaeda2ea031dd4806bc6f3e94659cafe
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2017-10-05 14:06:52 +0300

    TTALGO-472: Added build date to about dialog.

--- TickTrader.BotTerminal/Views/AboutDialogViewModel.cs
+++ TickTrader.BotTerminal/Views/AboutDialogViewModel.cs
@@ -5,18 +5,30 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TickTrader.BotTerminal.Extensions;
 
 namespace TickTrader.BotTerminal
 {
     class AboutDialogViewModel : Screen
     {
+        private static readonly string _version;
+        private static readonly string _buildDate;
+
+
         public string Version { get; private set; }
 
+
+        static AboutDialogViewModel()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            _version = entryAssembly.GetName().Version.ToString();
+            _buildDate = entryAssembly.GetLinkerTime().ToString("yyyy.MM.dd");
+        }
 
         public AboutDialogViewModel()
         {
             DisplayName = $"About - {EnvService.Instance.ApplicationName}";
-            Version = $"Version: {Assembly.GetEntryAssembly().GetName().Version}";
+            Version = $"Version: {_version} ({_buildDate})";
         }
 
 
