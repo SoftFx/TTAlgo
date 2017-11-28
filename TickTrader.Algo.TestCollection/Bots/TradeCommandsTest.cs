c193990a50ab152c478163906b728171d9e65e7c
Andrei Vasiuk <andrei.vasiuk@soft-fx.lv>
2017-11-28 15:03:10 +0300

    TTALGO-532: Fix verify prices function

--- TickTrader.Algo.TestCollection/Bots/TradeCommandsTest.cs
+++ TickTrader.Algo.TestCollection/Bots/TradeCommandsTest.cs
@@ -540,7 +540,7 @@ namespace TickTrader.Algo.TestCollection.Bots
 
         private bool EqualPrices(double a, double b)
         {
-            return (a - b <= Symbol.Point);
+            return a.E(b);
         }
 
         private void VerifyOrderDeleted(string orderId)
