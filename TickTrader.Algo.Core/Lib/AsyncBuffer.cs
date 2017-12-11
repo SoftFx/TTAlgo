ee4248de7b4fcdb2c2575a219e23afbcf97ac906
ahilevich <andrei.hilevich@soft-fx.lv>
2017-12-11 17:36:38 +0200

    Fix compile error

--- TickTrader.Algo.Core/Lib/AsyncBuffer.cs
+++ TickTrader.Algo.Core/Lib/AsyncBuffer.cs
@@ -19,15 +19,7 @@ namespace TickTrader.Algo.Core.Lib
         private T _waitingVal;
         private bool isClosed;
         private Exception _exception;
-#if DEBUG
-        private static int IdSeed = 1;
-        private int id = System.Threading.Interlocked.Increment(ref IdSeed);
 
-        private void Trace(string msg)
-        {
-            System.Diagnostics.Debug.WriteLine("AsyncBuffer[{0}] t={2} {1}", id, msg, System.Threading.Thread.CurrentThread.ManagedThreadId);
-        }
-#endif
         public bool Write(T item)
         {
             return WriteAsync(item).Result;
@@ -42,7 +34,7 @@ namespace TickTrader.Algo.Core.Lib
 
                 if (_reader != null)
                 {
-                    Trace("Write: Hit reader!");
+                    //Trace("Write: Hit reader!");
                     Task.Factory.StartNew(r => ((TaskCompletionSource<T>)r).SetResult(item), _reader);
                     _reader = null;
                     return TrueResult;
@@ -52,7 +44,7 @@ namespace TickTrader.Algo.Core.Lib
                     if (_writer != null)
                         throw new InvalidOperationException("Concurrent write!");
 
-                    Trace("Write: Wait for read!");
+                    //Trace("Write: Wait for read!");
 
                     _writer = new TaskCompletionSource<bool>();
                     _waitingVal = item;
@@ -110,7 +102,7 @@ namespace TickTrader.Algo.Core.Lib
             {
                 if (_writer != null)
                 {
-                    Trace("Read: Hit writer!");
+                    //Trace("Read: Hit writer!");
 
                     Task.Factory.StartNew(w => ((TaskCompletionSource<bool>)w).SetResult(true), _writer);
                     _writer = null;
@@ -128,7 +120,7 @@ namespace TickTrader.Algo.Core.Lib
                     if (_reader != null)
                         throw new InvalidOperationException("Concurrent read!");
 
-                    Trace("Read: Wait for read!");
+                    //Trace("Read: Wait for read!");
 
                     _reader = new TaskCompletionSource<T>();
                     return _reader.Task.ContinueWith(t =>
