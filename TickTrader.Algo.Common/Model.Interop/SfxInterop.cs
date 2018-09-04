f4ed36eb3c235d4f8bd253ab1805f237c56f0b4b
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2018-09-04 14:50:10 +0300

    Fixed Swap for net accoumts.

--- TickTrader.Algo.Common/Model.Interop/SfxInterop.cs
+++ TickTrader.Algo.Common/Model.Interop/SfxInterop.cs
@@ -902,7 +902,7 @@ namespace TickTrader.Algo.Common.Model
                 Symbol = p.Symbol,
                 Commission = p.Commission,
                 AgentCommission = p.AgentCommission,
-                Swap = p.Swap
+                Swap = p.Swap,
             };
         }
 
