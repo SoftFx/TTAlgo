3ef30a529f4aab82c814e18205d8f03d97e53015
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2018-09-20 12:22:34 +0300

    TTALGO-726: Added excluded fields to BotAgent message formatter.

--- TickTrader.Algo.Protocol/Grpc/MessageFormatter.cs
+++ TickTrader.Algo.Protocol/Grpc/MessageFormatter.cs
@@ -1,10 +1,17 @@
 ﻿using Google.Protobuf;
+using Google.Protobuf.Reflection;
 using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TickTrader.Algo.Protocol.Grpc
 {
     internal class MessageFormatter
     {
+        private static readonly string[] ExcludedFields = new[] { "password", "binary" };
+
+
         private JsonFormatter _formatter;
 
 
@@ -26,7 +33,7 @@ namespace TickTrader.Algo.Protocol.Grpc
         {
             if (LogMessages && logger != null)
             {
-                logger?.Info($"client > {request.GetType().Name}: {_formatter.Format(request)}");
+                logger?.Info($"client > {request.GetType().Name}: {Format(request)}");
             }
         }
 
@@ -34,7 +41,7 @@ namespace TickTrader.Algo.Protocol.Grpc
         {
             if (LogMessages && logger != null)
             {
-                logger?.Info($"client < {request.GetType().Name}: {_formatter.Format(request)}");
+                logger?.Info($"client < {request.GetType().Name}: {Format(request)}");
             }
         }
 
@@ -42,7 +49,7 @@ namespace TickTrader.Algo.Protocol.Grpc
         {
             if (LogMessages && logger != null)
             {
-                logger?.Info($"server < {request.GetType().Name}: {_formatter.Format(request)}");
+                logger?.Info($"server < {request.GetType().Name}: {Format(request)}");
             }
         }
 
@@ -50,7 +57,44 @@ namespace TickTrader.Algo.Protocol.Grpc
         {
             if (LogMessages && logger != null)
             {
-                logger?.Info($"server > {request.GetType().Name}: {_formatter.Format(request)}");
+                logger?.Info($"server > {request.GetType().Name}: {Format(request)}");
+            }
+        }
+
+
+        private string Format(IMessage msg)
+        {
+            var escapedFields = new List<Tuple<IFieldAccessor, object>>();
+
+            foreach (var field in msg.Descriptor.Fields.InFieldNumberOrder())
+            {
+                if (ExcludedFields.Any(f => field.Name.ToLower().Contains(f)))
+                {
+                    escapedFields.Add(new Tuple<IFieldAccessor, object>(field.Accessor, field.Accessor.GetValue(msg)));
+                    field.Accessor.SetValue(msg, GetDefaultValue(field.FieldType));
+                }
+            }
+
+            var res = _formatter.Format(msg);
+
+            foreach(var field in escapedFields)
+            {
+                field.Item1.SetValue(msg, field.Item2);
+            }
+
+            return res;
+        }
+
+        private object GetDefaultValue(FieldType type)
+        {
+            switch (type)
+            {
+                case FieldType.String:
+                    return "***";
+                case FieldType.Bytes:
+                    return ByteString.FromBase64("somebytesA==");
+                default:
+                    throw new ArgumentException("Unsupported field type");
             }
         }
     }
