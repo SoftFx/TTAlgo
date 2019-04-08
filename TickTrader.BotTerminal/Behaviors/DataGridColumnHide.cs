db2faf194c45c6f4679d345e2c68b3052390fe7a
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2019-04-08 12:51:54 +0300

    TTALGO-962: Update DependencyProperty DataGridColumnHide

--- TickTrader.BotTerminal/Behaviors/DataGridColumnHide.cs
+++ TickTrader.BotTerminal/Behaviors/DataGridColumnHide.cs
@@ -12,19 +12,28 @@ namespace TickTrader.BotTerminal
         public static readonly DependencyProperty IsShownProperty = DependencyProperty.Register("IsShown", typeof(bool),
     typeof(DataGridColumnHide), new PropertyMetadata(false, new PropertyChangedCallback(IsShownPropertyChanged)));
 
+        public static readonly DependencyProperty VisibilityProperty = DependencyProperty.Register("Visibility", typeof(Visibility),
+    typeof(DataGridColumnHide), new PropertyMetadata(Visibility.Collapsed));
+
+        public static readonly DependencyProperty ColumnKeyProperty = DependencyProperty.Register("ColumnKey", typeof(string),
+    typeof(DataGridColumnHide), new PropertyMetadata("Unknown", new PropertyChangedCallback(ColumnKeyPropertyChanged)));
+
         public bool IsShown
         {
             get { return (bool)GetValue(IsShownProperty); }
             set { SetValue(IsShownProperty, value); }
         }
 
-        public static readonly DependencyProperty VisibilityProperty = DependencyProperty.Register("Visibility", typeof(Visibility),
-            typeof(DataGridColumnHide), new PropertyMetadata(Visibility.Collapsed));
-
         public Visibility Visibility
         {
             get { return (Visibility)GetValue(VisibilityProperty); }
             private set { SetValue(VisibilityProperty, value); }
+        }
+
+        public string ColumnKey
+        {
+            get { return (string)GetValue(ColumnKeyProperty); }
+            set { SetValue(ColumnKeyProperty, value); }
         }
 
         private static void IsShownPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs a)
@@ -35,6 +44,13 @@ namespace TickTrader.BotTerminal
                 stateObj.Visibility = Visibility.Visible;
             else
                 stateObj.Visibility = Visibility.Collapsed;
+        }
+
+        private static void ColumnKeyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs a)
+        {
+            var stateObj = (DataGridColumnHide)d;
+
+            var key = a.NewValue.ToString();
         }
 
         protected override Freezable CreateInstanceCore()
