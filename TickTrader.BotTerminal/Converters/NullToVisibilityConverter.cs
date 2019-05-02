d0f70fe42a31a2922b3dbc066a9572dc3f869df4
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2019-05-02 14:14:28 +0300

    TTALGO-1008: Added hidden fields if its value is empty. Added StopPrice from Pendings ToolTips

---
+++ TickTrader.BotTerminal/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TickTrader.BotTerminal
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+                return string.IsNullOrEmpty(value.ToString()) ? Visibility.Collapsed : Visibility.Visible;
+            else
+                return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
