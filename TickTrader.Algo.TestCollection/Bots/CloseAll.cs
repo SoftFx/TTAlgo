e477790525a4ee932fdc579a66a3ec86b9264aff
ahilevich <andrei.hilevich@soft-fx.lv>
2019-08-01 12:20:00 +0300

    Update Close All script to work on Net account

--- TickTrader.Algo.TestCollection/Bots/CloseAll.cs
+++ TickTrader.Algo.TestCollection/Bots/CloseAll.cs
@@ -3,7 +3,7 @@ using TickTrader.Algo.Api;
 
 namespace TickTrader.Algo.TestCollection.Bots
 {
-    [TradeBot(DisplayName = "[T] Close All Positions Script", Version = "1.1", Category = "Test Orders",
+    [TradeBot(DisplayName = "[T] Close All Positions Script", Version = "1.2", Category = "Test Orders",
         SetupMainSymbol = false, Description = "Closes all positions for gross accounts")]
     public class CloseAll : TradeBot
     {
@@ -24,12 +24,21 @@ namespace TickTrader.Algo.TestCollection.Bots
             }
             else if (Account.Type == AccountTypes.Net)
             {
-                // TO DO : open opposite positions to close existing
+                foreach (var position in Account.NetPositions)
+                    await OpenOrderAsync(position.Symbol, OrderType.Market, Invert(position.Side), position.Volume, null, 1, null);
             }
             else
                 Print("This script works only for Net or Gross accounts!");
 
             Exit();
+        }
+
+        private static OrderSide Invert(OrderSide side)
+        {
+            if (side == OrderSide.Buy)
+                return OrderSide.Sell;
+            else
+                return OrderSide.Buy;
         }
     }
 }
