e90e37a854a7f36fc0f423f3394bd0a600ab14f5
ahilevich <andrei.hilevich@soft-fx.lv>
2019-09-25 13:31:26 +0300

    Profile generator

---
+++ tools/BotTerminalPorfileGenerator/BotTerminalPorfileGenerator/ProfileInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTerminalPorfileGenerator
+{
+    public class ProfileInfo
+    {
+        public List<ChartInfo> Charts { get; } = new List<ChartInfo>();
+        public string OpenedChartSymbol { get; set; }
+    }
+
+    public class ChartInfo
+    {
+        public string IndicatorNum { get; set; }
+        public string ChartId { get; set; }
+        public string Symbol { get; set; }
+    }
+}
---
+++ tools/BotTerminalPorfileGenerator/BotTerminalPorfileGenerator/Program.cs
@@ -0,0 +1,75 @@
+﻿using RazorEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTerminalPorfileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var template = LoadTemplate("Template.xml");
+            var templateKey = "ProfTemplate";
+
+            Razor.Compile(template, typeof(ProfileInfo), templateKey);
+
+            var model = new ProfileInfo();
+            var symbols = new string[] { "AUDCAD", "AUDCHF", "AUDJPY", "AUDNZD", "AUDUSD", "BCHUSD", "BTCCNH", "BTCEUR", "BTCGBP", "BTCJPY",
+                "BTCRUB", "BTCUSD", "CADCHF", "CADJPY", "CHFJPY", "EURAUD", "EURCAD", "EURCHF", "EURJPY", "EURNOK",
+                "EURNZD", "USDCNH", "EURSEK", "EURUSD", "GBPAUD", "GBPCAD", "GBPCHF", "GBPJPY", "GBPNZD", "GBPUSD",
+                "USDCAD", "USDJPY", "NZDCAD", "NZDCHF", "USDRUB", "NZDUSD", "BCHJPY", "USDCHF", "USDNOK", "USDSEK",};
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                var chartInfo = new ChartInfo();
+                chartInfo.IndicatorNum = i.ToString();
+                chartInfo.ChartId = "Chart" + i;
+                chartInfo.Symbol = symbols[i];
+                model.Charts.Add(chartInfo);
+            }
+
+            model.OpenedChartSymbol = symbols[0];
+
+            var result = Razor.Run(templateKey, model);
+
+            using (var file = File.Open("loadtest.tts.soft-fx.eu_1000.profile", FileMode.Create))
+            {
+                using (var writer = new StreamWriter(file))
+                    writer.Write(result);
+            }
+
+            Console.Read();
+
+        }
+
+        private static string LoadTemplate(string resourceName)
+        {
+            return LoadTemplate(Assembly.GetExecutingAssembly(), resourceName);
+        }
+
+        private static string LoadTemplate(Assembly assembly, string resourceName)
+        {
+            resourceName = FormatResourceName(assembly, resourceName);
+            using (Stream resourceStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resourceStream == null)
+                    return null;
+
+                using (StreamReader reader = new StreamReader(resourceStream))
+                    return reader.ReadToEnd();
+            }
+        }
+
+        private static string FormatResourceName(Assembly assembly, string resourceName)
+        {
+            return assembly.GetName().Name + "." + resourceName.Replace(" ", "_")
+                                                               .Replace("\\", ".")
+                                                               .Replace("/", ".");
+        }
+    }
+}
