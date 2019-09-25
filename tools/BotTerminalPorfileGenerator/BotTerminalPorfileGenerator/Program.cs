5960acc72617a9a6703ab3049788fb9be3fff679
ahilevich <andrei.hilevich@soft-fx.lv>
2019-09-25 13:38:24 +0300

    Add Net account profile

--- tools/BotTerminalPorfileGenerator/BotTerminalPorfileGenerator/Program.cs
+++ tools/BotTerminalPorfileGenerator/BotTerminalPorfileGenerator/Program.cs
@@ -11,18 +11,31 @@ namespace BotTerminalPorfileGenerator
 {
     class Program
     {
+        const string templateKey = "ProfTemplate";
+
         static void Main(string[] args)
         {
             var template = LoadTemplate("Template.xml");
-            var templateKey = "ProfTemplate";
-
+            
             Razor.Compile(template, typeof(ProfileInfo), templateKey);
 
-            var model = new ProfileInfo();
             var symbols = new string[] { "AUDCAD", "AUDCHF", "AUDJPY", "AUDNZD", "AUDUSD", "BCHUSD", "BTCCNH", "BTCEUR", "BTCGBP", "BTCJPY",
                 "BTCRUB", "BTCUSD", "CADCHF", "CADJPY", "CHFJPY", "EURAUD", "EURCAD", "EURCHF", "EURJPY", "EURNOK",
                 "EURNZD", "USDCNH", "EURSEK", "EURUSD", "GBPAUD", "GBPCAD", "GBPCHF", "GBPJPY", "GBPNZD", "GBPUSD",
                 "USDCAD", "USDJPY", "NZDCAD", "NZDCHF", "USDRUB", "NZDUSD", "BCHJPY", "USDCHF", "USDNOK", "USDSEK",};
+
+            var netSymbols = symbols.Select(MakeNetSymbol).ToArray();
+
+            GenerateProfile("gross-acc.profile", symbols);
+            GenerateProfile("net-acc.profile", netSymbols);
+
+            Console.Read();
+
+        }
+
+        private static void GenerateProfile(string filePath, string[] symbols)
+        {
+            var model = new ProfileInfo();
 
             for (int i = 0; i < symbols.Length; i++)
             {
@@ -37,14 +50,11 @@ namespace BotTerminalPorfileGenerator
 
             var result = Razor.Run(templateKey, model);
 
-            using (var file = File.Open("loadtest.tts.soft-fx.eu_1000.profile", FileMode.Create))
+            using (var file = File.Open(filePath, FileMode.Create))
             {
                 using (var writer = new StreamWriter(file))
                     writer.Write(result);
             }
-
-            Console.Read();
-
         }
 
         private static string LoadTemplate(string resourceName)
@@ -70,6 +80,13 @@ namespace BotTerminalPorfileGenerator
             return assembly.GetName().Name + "." + resourceName.Replace(" ", "_")
                                                                .Replace("\\", ".")
                                                                .Replace("/", ".");
+        }
+
+        private static string MakeNetSymbol(string symbol)
+        {
+            var c1 = symbol.Substring(0, 3);
+            var c2 = symbol.Substring(3, 3);
+            return c1 + "/" + c2;
         }
     }
 }
