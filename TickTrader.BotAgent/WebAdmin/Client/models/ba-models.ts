45d7a45621d17c6f5ab8a712112fb87eb81c1397
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2019-11-28 13:29:31 +0300

    TTALGO-384: Fixed package removal.

--- TickTrader.BotAgent/WebAdmin/Client/models/ba-models.ts
+++ TickTrader.BotAgent/WebAdmin/Client/models/ba-models.ts
@@ -40,6 +40,7 @@ export class AccountModel implements Serializable<AccountModel> {
 
 export class PackageModel implements Serializable<PackageModel> {
     public Name: string;
+    public DisplayName: string;
     public Created: Date;
     public IsValid: boolean;
     public Plugins: PluginModel[];
@@ -52,6 +53,7 @@ export class PackageModel implements Serializable<PackageModel> {
         this.Created = input.Created;
         this.IsValid = input.IsValid;
         this.Name = input.Name;
+        this.DisplayName = input.DisplayName;
         this.Plugins = input.Plugins ? input.Plugins.map(p => new PluginModel(input.Name).Deserialize(p)) : input.Plugins;
 
         return this;
--- TickTrader.BotAgent/WebAdmin/Server/Dto/PackageDto.cs
+++ TickTrader.BotAgent/WebAdmin/Server/Dto/PackageDto.cs
@@ -5,6 +5,7 @@ namespace TickTrader.BotAgent.WebAdmin.Server.Dto
     public class PackageDto
     {
         public string Name { get; set; }
+        public string DisplayName { get; set; }
         public DateTime Created { get; set; }
         public PluginDto[] Plugins { get; set; }
         public bool IsValid { get; set; }
