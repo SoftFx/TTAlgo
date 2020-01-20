036711c68bd15dbeae9072d3c9a982d0a07222e5
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2020-01-20 16:06:28 +0300

    TTALGO-1324: Main Symbol token has been added from terminal

---
+++ TickTrader.Algo.TestCollection/Bots/MainTokenTestBot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TickTrader.Algo.Api;
+
+namespace TickTrader.Algo.TestCollection.Bots
+{
+
+    [TradeBot(DisplayName = "[T] Main Token Test Bot", Version = "1.0", Category = "Test Bot Routine")]
+    public class MainTokenTestBot : TradeBot
+    {
+        [Input]
+        public DataSeries Token { get; set; }
+
+        protected override void OnQuote(Quote quote)
+        {
+            var token = Token[0];
+
+            string str = $"Token: {token}, Quote: {quote.Bid}";
+
+            Print(str);
+
+            if (token != quote.Bid)
+                PrintError($"Error: {str}");
+        }
+    }
+}
