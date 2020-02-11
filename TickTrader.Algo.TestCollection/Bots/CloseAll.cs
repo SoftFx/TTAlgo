24e66cd83c4fc90ea4250c6eaf4c6c4ae1b4042b
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2020-02-11 12:55:17 +0300

    TTALGO-1326: Close All Position Script has been improved

--- TickTrader.Algo.TestCollection/Bots/CloseAll.cs
+++ TickTrader.Algo.TestCollection/Bots/CloseAll.cs
@@ -1,15 +1,26 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using TickTrader.Algo.Api;
 
 namespace TickTrader.Algo.TestCollection.Bots
 {
-    [TradeBot(DisplayName = "[T] Close All Positions Script", Version = "1.2", Category = "Test Orders",
+    [TradeBot(DisplayName = "[T] Close Positions Script", Version = "1.3", Category = "Test Orders",
         SetupMainSymbol = false, Description = "Closes all positions for gross accounts")]
     public class CloseAll : TradeBot
     {
+        [Parameter]
+        public string PositionId { get; set; }
+
         [Parameter(DisplayName = "Target Instance Id", DefaultValue = "")]
         public string TargetInstanceId { get; set; }
 
+        [Parameter(DisplayName = "Close volume", DefaultValue = null)]
+        public double? Volume { get; set; }
+
+        protected override void Init()
+        {
+            var bal = Account.Balance;
+        }
 
         protected async override void OnStart()
         {
@@ -19,13 +30,16 @@ namespace TickTrader.Algo.TestCollection.Bots
                 ? Account.Orders.Where(o => o.Type == OrderType.Position).ToList()
                 : Account.Orders.Where(o => o.Type == OrderType.Position && o.InstanceId == TargetInstanceId).ToList();
 
+                if (!string.IsNullOrEmpty(PositionId))
+                    positions = positions.Where(o => o.Id == PositionId).ToList();
+
                 foreach (var pos in positions)
-                    await CloseOrderAsync(pos.Id);
+                    await CloseOrderAsync(pos.Id, Volume);
             }
             else if (Account.Type == AccountTypes.Net)
             {
                 foreach (var position in Account.NetPositions)
-                    await OpenOrderAsync(position.Symbol, OrderType.Market, Invert(position.Side), position.Volume, null, 1, null);
+                    await OpenOrderAsync(position.Symbol, OrderType.Market, Invert(position.Side), Volume ?? position.Volume, null, 1, null);
             }
             else
                 Print("This script works only for Net or Gross accounts!");
