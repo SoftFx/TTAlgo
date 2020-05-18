7f46006f9a9fac094d8365d0c9c444500dfff6ed
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2020-05-18 15:50:43 +0300

    TTALGO-1391,1392: Slippage implamentation part 2

---
+++ TickTrader.BotTerminal/Converters/DoubleRoundConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TickTrader.BotTerminal
+{
+    public class DoubleRoundConverter : IValueConverter
+    {
+        private static readonly string _template = $"0.{new string('#', 10)}";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is double d ? d.ToString(_template) : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
