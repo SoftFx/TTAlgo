7429c1ef6450a8ef63b631235ba813099d5d5158
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2020-07-10 11:37:18 +0300

    RpcMessage has been refactored

--- src/csharp/core/TickTrader.Algo.Rpc/RpcMessage.cs
+++ src/csharp/core/TickTrader.Algo.Rpc/RpcMessage.cs
@@ -8,51 +8,30 @@ namespace TickTrader.Algo.Rpc
     {
         public static string GenerateCallId() => Guid.NewGuid().ToString("N");
 
+
         public static RpcMessage Request(IMessage payload) => Request(Any.Pack(payload));
 
-        public static RpcMessage Request(Any payload)
-        {
-            return new RpcMessage { CallId = GenerateCallId(), Flags = RpcFlags.Request, Payload = payload };
-        }
+        public static RpcMessage Request(IMessage payload, string callId) => Request(Any.Pack(payload), callId);
 
-        public static RpcMessage Request(IMessage payload, string callId)
-        {
-            return new RpcMessage { CallId = callId, Flags = RpcFlags.Request, Payload = Any.Pack(payload) };
-        }
+        public static RpcMessage Request(Any payload) => GetRpcMessage(GenerateCallId(), RpcFlags.Request, payload);
 
-        public static RpcMessage Request(Any payload, string callId)
-        {
-            return new RpcMessage { CallId = callId, Flags = RpcFlags.Request, Payload = payload };
-        }
+        public static RpcMessage Request(Any payload, string callId) => GetRpcMessage(callId, RpcFlags.Request, payload);
 
-        public static RpcMessage Response(string requestCallId, IMessage payload)
-        {
-            return new RpcMessage { CallId = requestCallId, Flags = RpcFlags.Response, Payload = Any.Pack(payload) };
-        }
 
-        public static RpcMessage Response(string requestCallId, Any payload)
-        {
-            return new RpcMessage { CallId = requestCallId, Flags = RpcFlags.Response, Payload = payload };
-        }
+        public static RpcMessage Response(string requestCallId, IMessage payload) => Response(requestCallId, Any.Pack(payload));
 
-        public static RpcMessage Notification(IMessage payload)
-        {
-            return new RpcMessage { CallId = string.Empty, Flags = RpcFlags.Notification, Payload = Any.Pack(payload) };
-        }
+        public static RpcMessage Response(string requestCallId, Any payload) => GetRpcMessage(requestCallId, RpcFlags.Response, payload);
 
-        public static RpcMessage Notification(Any payload)
-        {
-            return new RpcMessage { CallId = string.Empty, Flags = RpcFlags.Notification, Payload = payload };
-        }
 
-        public static RpcMessage Notification(IMessage payload, string callId)
-        {
-            return new RpcMessage { CallId = callId, Flags = RpcFlags.Notification, Payload = Any.Pack(payload) };
-        }
+        public static RpcMessage Notification(Any payload) => Notification(payload, string.Empty);
 
-        public static RpcMessage Notification(Any payload, string callId)
-        {
-            return new RpcMessage { CallId = callId, Flags = RpcFlags.Notification, Payload = payload };
-        }
+        public static RpcMessage Notification(IMessage payload) => Notification(Any.Pack(payload), string.Empty);
+
+        public static RpcMessage Notification(IMessage payload, string callId) => Notification(Any.Pack(payload), callId);
+
+        public static RpcMessage Notification(Any payload, string callId) => GetRpcMessage(callId, RpcFlags.Notification, payload);
+
+
+        private static RpcMessage GetRpcMessage(string callId, RpcFlags flag, Any payload) => new RpcMessage { CallId = callId, Flags = flag, Payload = payload };
     }
 }
