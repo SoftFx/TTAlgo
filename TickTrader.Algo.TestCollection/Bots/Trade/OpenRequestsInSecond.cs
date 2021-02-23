6e189f47fadb131caf1ce7f59cc713f31a66a924
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2021-02-23 10:49:12 +0300

    BotTerminal: OpenOrderPerSecond bot has been improved

--- TickTrader.Algo.TestCollection/Bots/Trade/OpenRequestsInSecond.cs
+++ TickTrader.Algo.TestCollection/Bots/Trade/OpenRequestsInSecond.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using TickTrader.Algo.Api;
 
 namespace TickTrader.Algo.TestCollection.Bots.Trade
@@ -6,19 +7,40 @@ namespace TickTrader.Algo.TestCollection.Bots.Trade
     [TradeBot(DisplayName = "[T] Open Requests In Second", Version = "1.0")]
     class OpenRequestsInSecond : TradeBot
     {
+        private Stopwatch _stopwatch;
+        private bool _run = false;
+
         [Parameter(DefaultValue = 100)]
         public int RequestCount { get; set; }
 
-        protected override void OnStart()
+        protected override void Init()
         {
-            var st = Stopwatch.StartNew();
+            _stopwatch = new Stopwatch();
+            _run = true;
+        }
 
-            for (int i = 0; i < RequestCount; ++i)
-                OpenOrderAsync(OpenOrderRequest.Template.Create().WithParams("EURUSD", OrderSide.Buy, OrderType.Limit, 1.0, 0.1, null).MakeRequest());
+        protected async override void OnStart()
+        {
+            while (_run)
+            {
+                await Delay(1000 - DateTime.Now.Millisecond);
 
-            st.Stop();
+                var side = DateTime.Now.Second % 2 == 0 ? OrderSide.Buy : OrderSide.Sell;
 
-            Status.WriteLine($"Total time: {st.ElapsedTicks / 1e4} ms");
+                _stopwatch.Restart();
+
+                for (int i = 0; i < RequestCount; ++i)
+                    OpenOrderAsync(OpenOrderRequest.Template.Create().WithParams(Symbol.Name, side, OrderType.Market, 1.0, 0.1, null).MakeRequest());
+
+                _stopwatch.Stop();
+
+                Status.WriteLine($"Total time: {_stopwatch.ElapsedTicks / 1e4} ms");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _run = false;
         }
     }
 }
