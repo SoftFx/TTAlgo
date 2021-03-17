a8b854d1383531ddbc890eb7581bc0943981524d
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2021-03-17 14:46:02 +0300

    Removed AlgoProtocol from solution. Fixed dataflow references.

--- TickTrader.Algo.Protocol/Infrastructure/IFileProgressListener.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TickTrader.Algo.Protocol
-{
-    public interface IFileProgressListener
-    {
-        void Init(long initialProgress);
-
-        void IncrementProgress(long progressValue);
-    }
-}
