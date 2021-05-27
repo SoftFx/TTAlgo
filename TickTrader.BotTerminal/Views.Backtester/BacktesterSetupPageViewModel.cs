fc7585bcb07d9aac7930ef77d12efb09acaad027
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2021-05-27 14:43:45 +0300

    Some files has been removed

--- TickTrader.BotTerminal/Views.Backtester/BacktesterSetupPageViewModel.cs
+++ TickTrader.BotTerminal/Views.Backtester/BacktesterSetupPageViewModel.cs
@@ -523,6 +523,32 @@ namespace TickTrader.BotTerminal
         #endregion IAlgoSetupContext
     }
 
+    public class OptionalItem<T> : ObservableObject
+    {
+        private bool _enabled;
+
+        public OptionalItem(T value, bool enabled = true)
+        {
+            Value = value;
+            _enabled = enabled;
+        }
+
+        public T Value { get; }
+
+        public bool IsEnabled
+        {
+            get => _enabled;
+            set
+            {
+                if (_enabled != value)
+                {
+                    _enabled = value;
+                    NotifyOfPropertyChange(nameof(IsEnabled));
+                }
+            }
+        }
+    }
+
     public enum TesterModes
     {
         Backtesting,
