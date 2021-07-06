789d7cb371ef22ef5b351840f1883134ee3c9e0e
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2021-07-06 16:44:39 +0300

    Update saved state serialization.

--- src/csharp/core/TickTrader.Algo.Server/Persistence/AccountSavedState.cs
+++ src/csharp/core/TickTrader.Algo.Server/Persistence/AccountSavedState.cs
@@ -35,7 +35,7 @@ namespace TickTrader.Algo.Server.Persistence
         public AccountCreds UnpackCreds()
         {
             if (CredsUri == AccountCreds.Descriptor.FullName)
-                return AccountCreds.Parser.ParseFrom(ByteString.CopyFromUtf8(CredsData));
+                return AccountCreds.Parser.ParseFrom(ByteString.FromBase64(CredsData));
 
             return null;
         }
@@ -43,7 +43,7 @@ namespace TickTrader.Algo.Server.Persistence
         public void PackCreds(AccountCreds creds)
         {
             CredsUri = AccountCreds.Descriptor.FullName;
-            CredsData = creds.ToByteString().ToStringUtf8();
+            CredsData = creds.ToByteString().ToBase64();
         }
     }
 }
