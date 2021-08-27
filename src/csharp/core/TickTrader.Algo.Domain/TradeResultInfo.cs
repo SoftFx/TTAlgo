0c2e204af927317963a88124e6df50f8080fb267
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2021-08-27 18:38:39 +0300

    Fixed error on reconnect.

--- src/csharp/core/TickTrader.Algo.Domain/TradeResultInfo.cs
+++ src/csharp/core/TickTrader.Algo.Domain/TradeResultInfo.cs
@@ -4,7 +4,7 @@ namespace TickTrader.Algo.Domain
 {
     public partial class TradeResultInfo
     {
-        public DateTime? TransactionTime => (ResultingOrder.Modified ?? ResultingOrder.Created)?.ToDateTime();
+        public DateTime? TransactionTime => (ResultingOrder?.Modified ?? ResultingOrder?.Created)?.ToDateTime();
 
 
         public TradeResultInfo(OrderExecReport.Types.CmdResultCode resultCode, OrderInfo resultingOrder)
