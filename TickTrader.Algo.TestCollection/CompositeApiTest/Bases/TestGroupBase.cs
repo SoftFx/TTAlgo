02be53dd35aed3f6605341ba8510ecaafa75228b
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2021-10-07 17:41:49 +0300

    CompositeTests: Some improvements have been added

---
+++ TickTrader.Algo.TestCollection/CompositeApiTest/Bases/TestGroupBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TickTrader.Algo.TestCollection.CompositeApiTest
+{
+    internal abstract class TestGroupBase
+    {
+        private readonly Stopwatch _groupWatcher = new Stopwatch();
+
+
+        internal event Action<GroupTestResult> TestsFinishedEvent;
+
+
+        internal async Task Run()
+        {
+            _groupWatcher.Start();
+
+            await RunTests();
+
+            _groupWatcher.Stop();
+
+            TestsFinishedEvent?.Invoke(new GroupTestResult());
+        }
+
+        protected abstract Task RunTests();
+    }
+}
---
+++ TickTrader.Algo.TestCollection/CompositeApiTest/GroupTestResult.cs
@@ -0,0 +1,21 @@
+﻿namespace TickTrader.Algo.TestCollection.CompositeApiTest
+{
+    internal readonly struct GroupTestResult
+    {
+        public int TotalMilliseconds { get; }
+
+        public int TestCount { get; }
+
+        public int ErrorCount { get; }
+
+        public int SuccCount => TestCount - ErrorCount;
+
+
+        public GroupTestResult(int milliseconds, int testCount, int errors)
+        {
+            TotalMilliseconds = milliseconds;
+            TestCount = testCount;
+            ErrorCount = errors;
+        }
+    }
+}
