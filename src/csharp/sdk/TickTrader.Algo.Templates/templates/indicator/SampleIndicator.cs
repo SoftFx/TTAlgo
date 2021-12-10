f963e3c121cf518779af5188203a83f5c270905d
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2021-12-10 14:39:41 +0300

    Added .NET CLI templates package.

---
+++ src/csharp/sdk/TickTrader.Algo.Templates/templates/indicator/SampleIndicator.cs
@@ -0,0 +1,13 @@
+﻿using TickTrader.Algo.Api;
+
+namespace SampleIndicator
+{
+    [Indicator(Category = "My indicators", DisplayName = "SampleIndicator", Version="1.0",
+        Description = "My own SampleIndicator")]
+    public class SampleIndicator : Indicator
+    {
+        protected override void Calculate(bool isNewBar)
+        {
+        }
+    }
+}
---
+++ src/csharp/sdk/TickTrader.Algo.Templates/templates/tradebot/SampleTradeBot.cs
@@ -0,0 +1,14 @@
+﻿using TickTrader.Algo.Api;
+
+namespace SampleTradeBot
+{
+    [TradeBot(Category = "My bots", DisplayName = "SampleTradeBot", Version="1.0",
+        Description = "My own SampleTradeBot")]
+    public class SampleTradeBot : TradeBot
+    {
+        protected override void OnStart()
+        {
+            Status.WriteLine("Hello world!");
+        }
+    }
+}
