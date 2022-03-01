9b7fe08b66b833e6e7902147f8c1999e3508cc7f
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2022-03-01 16:55:06 +0300

    Speed counter bot has been added

---
+++ src/csharp/api/TickTrader.Algo.TestCollection/Bots/Performance/SpeedCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using TickTrader.Algo.Api;
+
+namespace TickTrader.Algo.TestCollection
+{
+    [TradeBot(DisplayName = "[P] Tester Speed Counter", Version = "1.0", Category = "Backtester")]
+    public class TesterSpeedCounter : TradeBot
+    {
+        private ulong _totalQuotesCount;
+        private DateTime _startTime;
+
+        protected override void OnStart()
+        {
+            _startTime = DateTime.UtcNow;
+            _totalQuotesCount = 0;
+
+            Print($"Folder - {Enviroment.BotDataFolder}");
+        }
+
+        protected override void OnQuote(Quote quote)
+        {
+            _totalQuotesCount++;
+        }
+
+        protected override void OnStop()
+        {
+            var workingTime = DateTime.UtcNow - _startTime;
+            var fileName = Path.Combine(Enviroment.BotDataFolder, "AlgoTester.csv");
+            var speed = _totalQuotesCount / workingTime.TotalMilliseconds;
+
+            bool fileExist = System.IO.File.Exists(fileName);
+
+            using (var fs = new FileStream(fileName, FileMode.Append))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    if (!fileExist)
+                        sw.WriteLine($"Symbol;Time(ms);Quotes;Speed");
+
+                    sw.WriteLine($"{Symbol.Name};{workingTime.TotalMilliseconds:F4};{_totalQuotesCount};{speed:F10}");
+                }
+            }
+        }
+    }
+}
