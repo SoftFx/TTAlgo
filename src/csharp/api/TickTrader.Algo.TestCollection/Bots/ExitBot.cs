2f440ab7a1a455bc85aaf970096309b5903c8c59
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2022-07-08 18:33:07 +0300

    Fix exit bot to support new logic.

--- src/csharp/api/TickTrader.Algo.TestCollection/Bots/ExitBot.cs
+++ src/csharp/api/TickTrader.Algo.TestCollection/Bots/ExitBot.cs
@@ -5,7 +5,7 @@ namespace TickTrader.Algo.TestCollection.Bots
     public enum ExitVariants { Init, OnStart, OnQuote, OnStop }
 
     [TradeBot(DisplayName = "[T] Exit Bot", Version = "1.0", Category = "Test Bot Routine",
-        SetupMainSymbol = false, Description = "Call Exit function on specified moment of bot lifecycle")]
+        Description = "Call Exit function on specified moment of bot lifecycle")]
     public class ExitBot : TradeBot
     {
         [Parameter(DisplayName = "Exit on")]
