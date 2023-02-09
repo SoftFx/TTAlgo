a699a29cf0646ea141b94d910c5461fa86410034
Pavel Kindruk <pavel.kindruk@soft-fx.lv>
2023-02-09 19:32:37 +0300

    Tweak SymbolList a bit. Fix some XAML binding errors.

--- TickTrader.BotTerminal/Converters/Machinarium.Converters/DateTimeToUtc.cs
+++ TickTrader.BotTerminal/Converters/Machinarium.Converters/DateTimeToUtc.cs
@@ -5,7 +5,7 @@ namespace TickTrader.BotTerminal.Converters.Machinarium.Converters
 {
     internal sealed class DateTimeToUtc : IDisplayValueConverter<DateTime?>
     {
-        public const string FullTimeFormat = "dd-MMM-yyyy HH:mm:ss.fff";
+        public const string FullTimeFormat = "HH:mm:ss.fff dd-MM-yyyy";
 
         public string Convert(DateTime? val) => val?.ToUniversalTime().ToString(FullTimeFormat) ?? string.Empty;
     }
